c3dd9be0d18dc7a33387dbf83ce570e60f7cc659
Jon Lu <jontlu@me.com>
2021-01-31 21:40:18 -0500

    added block validation

--- coopercoin/app/src/main/java/coopercoin/test.java
+++ coopercoin/app/src/main/java/coopercoin/test.java
@@ -5,7 +5,18 @@ import java.util.ArrayList;
 public class test {
 	public static ArrayList<Block> blockchain = new ArrayList<Block>();
 	public static int difficulty = 4;
-
+	public static Boolean isBlockchainValid(){
+		boolean flag = true;
+		String hashTarget = new String(new char[difficulty]).replace('\0', '0');
+		for (int i = 0; i < blockchain.size(); i++) {
+			String prevHash = i==0 ? "0" : blockchain.get(i-1).blockHash;
+			flag = blockchain.get(i).blockHash.equals(blockchain.get(i).getHash())
+				&& prevHash.equals(blockchain.get(i).prevHash)
+			  	&& blockchain.get(i).getHash().substring(0, difficulty).equals(hashTarget);
+				if (!flag) break;
+		}
+		return flag;
+	}
 	public static void main(String[] args) {
 		for(int n = 0; n < 4; n++){
 			String nString = n + "";
@@ -17,6 +28,7 @@ public class test {
 			}
 			blockchain.get(n).mineBlock(difficulty);
 			System.out.println("Block "+n+" mined!\t"+blockchain.get(n).blockHash);
+			System.out.println("Blockchain is Valid: " + isBlockchainValid() + "\n");
 		}
 	}
 }
