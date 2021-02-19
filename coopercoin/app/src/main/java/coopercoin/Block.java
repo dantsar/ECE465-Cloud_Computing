82c274a05e44d58d0d9f7f0831f6cba8b479f05b
Daniel Tsarev <danieltsarev@gmail.com>
2021-02-18 23:12:43 -0500

    added Block functionality

--- coopercoin/app/src/main/java/coopercoin/Block.java
+++ coopercoin/app/src/main/java/coopercoin/Block.java
@@ -4,36 +4,21 @@ import java.util.ArrayList;
 
 public class Block
 {
-
     public BlockHeader header;
-    public String blockHash = null;
-    public String prevHash;
-//    public String rootHash; //hash of the the transaction(s)
-    private long date; 
     public ArrayList<Tx> txMade;
-    public int nonce; 
     public final int txLimit = 4;
-    public int blocksize;
+    public int blocksize = 0;
 
+    /* only really used for the genesis tx */
     public Block(Tx txMade, String prevHash){
         this.header = new BlockHeader(prevHash);
-
-
-        this.prevHash = prevHash;
-        this.date = System.currentTimeMillis();
-        //this.blockHash = getHash();
-        this.txMade.add(txMade);
-        this.blocksize = 1;
-//        setBlockTxHash();
+        this.blocksize = 0;
     }
 
-    // TO DO: implement merkle tree for more transactions */
-//    // public void setBlockTxHash(){
-//        this.rootHash = HashUtil.strToHexHash(HashUtil.hexFromKey(txMade.sender)+
-//                                              HashUtil.hexFromKey(txMade.receiver)+
-//                                              Float.toString(txMade.amtSent)+
-//                                              txMade.txId);
-//    // }
+    public Block(Tx txMade, BlockHeader prevHeader){
+        this.header = new BlockHeader(prevHeader.getBlockHash());
+        this.blocksize = 0;
+    }
 
     /* for now, only works if the txLimit is a power of two :^) */
     public void setRootHash(){
@@ -63,28 +48,20 @@ public class Block
         return true;
     }
 
-    // public String getHash(){
-    //     String preImage = prevHash + Long.toString(date) + rootHash + Integer.toString(nonce);
-    //     try{
-    //         return HashUtil.strToHexHash(preImage);
-    //     }catch(Exception err) {
-    //         System.err.println("error getting hash");
-    //         System.err.println(preImage);
-	// 		throw new RuntimeException(err);
-    //     }
-    // }
-
-    synchronized public void setBlockHash(String hash){
-        if(blockHash == null){
-            blockHash = hash;
-        }
+    public void setHeader(BlockHeader header){
+        this.header = header;
     }
 
-//    public void mineBlock(int prefix) {
-//        String prefixString = new String(new char[prefix]).replace('\0', '0');
-//        while (!blockHash.substring(0, prefix).equals(prefixString)) {
-//            nonce++;
-//            blockHash = getHash();
-//        }
-//    }
+    public BlockHeader getHeader(){
+        return this.header;
+    }
+
+    public String getHash(){
+        return header.getBlockHash();
+    }
+    // synchronized public void setBlockHash(String hash){
+    //     if(blockHash == null){
+    //         blockHash = hash;
+    //     }
+    // }
 }
--- coopercoin/app/src/main/java/coopercoin/BlockHeader.java
+++ coopercoin/app/src/main/java/coopercoin/BlockHeader.java
@@ -9,6 +9,7 @@ public class BlockHeader
     public String prevHash;
     public String rootHash;
     public int nonce;
+
     private long date; 
 
 
@@ -16,11 +17,25 @@ public class BlockHeader
         this.prevHash = prevHash;
     }
 
+    BlockHeader(BlockHeader copy){
+        this.prevHash = copy.prevHash;
+        this.rootHash = copy.rootHash;
+        this.date = copy.date;
+    }
+
     public void setRootHash(String rootHash){
         this.rootHash = rootHash;
         this.date = new Date().getTime();
     }
 
+    public String getRootHash(){
+        if(rootHash == null){
+            return null;
+        }
+        return rootHash;
+    }
+
+    /* returns the hash with the provided nonce */
     public String testHash(int nonce){
         String preImage = prevHash + Long.toString(date) + rootHash + Integer.toString(nonce);
         return HashUtil.strToHexHash(preImage);
